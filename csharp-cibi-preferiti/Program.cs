9c24cd9de3dbb9c932767756b105ea47830f98e5
Blaser993 <corradodepinto.design@gmail.com>
2023-09-05 16:04:26 +0200

    refact

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -6,13 +6,17 @@ string[] cibi = { "uova sode","parmigiana","pizza","tiramisù","trippa al sugo",
 Console.WriteLine("Ecco la tua classifica di cibi preferiti:");
 
 
-Console.WriteLine("Ci sono " + cibi.Length + " cibi preferiti nella tua lista");
+Console.WriteLine($"Ci sono  {cibi.Length}  cibi preferiti nella tua lista");
 
 for (int i = 0; i < cibi.Length; i++)
 {
-    Console.WriteLine(i + " - " + cibi[i]);
+    Console.WriteLine($"{i} - {cibi[i]}");
 }
 
-Console.WriteLine("Il tuo cibo preferito in assoluto è: " + cibi[0]);
+Console.WriteLine($"Il tuo cibo preferito in assoluto è: {cibi[0]}");
 
-Console.WriteLine("il tuo peggior cibo preferito è: " + cibi[cibi.Length - 1]);
+Console.WriteLine( $"Il tuo peggior cibo preferito è: {cibi[cibi.Length - 1]}");
+
+//BONUS
+
+
