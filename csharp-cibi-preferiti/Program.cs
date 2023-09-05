83d4c4507dd46ab4c6b31d0604dd801f7411245d
Blaser993 <corradodepinto.design@gmail.com>
2023-09-05 15:15:05 +0200

    Secondo risultato

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -3,3 +3,12 @@ Console.WriteLine("Hello, World!");
 
 string[] cibi = { "Uova sode","Parmigiana","Pizza","Tiramisù","Trippa al sugo","Orecchiette con le cime di rapa" };
 
+Console.WriteLine("Ecco la tua classifica di cibi preferiti:");
+
+
+Console.WriteLine("Ci sono " + cibi.Length + " cibi preferiti nella tua lista");
+
+for (int i = 0; i < cibi.Length; i++)
+{
+    Console.WriteLine(i + " - " + cibi[i]);
+}
