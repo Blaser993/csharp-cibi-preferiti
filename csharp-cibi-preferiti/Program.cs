1b4e9e3cf432f309cab7a071ce4e7bcad783c310
Blaser993 <corradodepinto.design@gmail.com>
2023-09-05 15:02:07 +0200

    creo array di cibi

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string[] cibi = { "Uova sode","Parmigiana","Pizza","Tiramisù","Trippa al sugo","Orecchiette con le cime di rapa" };
+
