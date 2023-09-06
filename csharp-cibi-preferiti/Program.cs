cb0f61775027766b47ae7f0867112cdb292efa3e
Blaser993 <corradodepinto.design@gmail.com>
2023-09-06 10:03:53 +0200

    Correzione

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,16 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string[] cibi = { "uova sode","parmigiana","tiramisù","trippa al sugo","orecchiette con le cime di rapa" };
+string[] cibi = { "uova sode","parmigiana","tiramisù","pizza","trippa al sugo","orecchiette con le cime di rapa" };
 
 Console.WriteLine("Ecco la tua classifica di cibi preferiti:");
-
 
 Console.WriteLine($"Ci sono  {cibi.Length}  cibi preferiti nella tua lista.");
 
 for (int i = 0; i < cibi.Length; i++)
 {
-    Console.WriteLine($"{i} - {cibi[i]}");
+    Console.WriteLine($"{i + 1} - {cibi[i]}");
 }
 
 Console.WriteLine($"Il tuo cibo preferito in assoluto è: {cibi[0]}.");
@@ -19,28 +18,18 @@ Console.WriteLine( $"Il tuo peggior cibo preferito è: {cibi[cibi.Length - 1]}."
 
 //BONUS
 
-int ciboMediano = 0;
-
-if (cibi.Length % 2 == 0)
+//caso dispari
+if (cibi.Length % 2 == 1)
 {
-     ciboMediano = cibi.Length / 2;
-    Console.WriteLine($"Il tuo cibo mediamente preferito è: {cibi[ciboMediano]}.");
+    int indiceCiboMediano = cibi.Length / 2 ;
+    Console.WriteLine($"Il tuo cibo mediamente preferito è: {cibi[indiceCiboMediano]}.");
 }
-else if (cibi.Length % 2 == 1)
+//caso pari
+else 
 {
-    double lunghezzaMedia = Convert.ToDouble(cibi.Length)/2;
-
-    //Console.WriteLine($"lunghezza media = {lunghezzaMedia}");
-
-    lunghezzaMedia = Math.Round(lunghezzaMedia);
-
-    int lunghezzaMedia2 = Convert.ToInt32(lunghezzaMedia);
-
-    int lunghezzaMedia1 = Convert.ToInt32(lunghezzaMedia) - 1;
-
-    //Console.WriteLine($"lunghezza media = {lunghezzaMedia}");
-
-    Console.WriteLine($"I tuoi cibi mediamente preferiti sono: {cibi[lunghezzaMedia2]} e {cibi[lunghezzaMedia1]}.");
+    int secondoIndiceCiboMediano = cibi.Length / 2;
+    int primoIndiceCiboMediano = secondoIndiceCiboMediano - 1;
+    Console.WriteLine($"I tuoi cibi mediamente preferiti sono: {cibi[primoIndiceCiboMediano]} e {cibi[secondoIndiceCiboMediano]}.");
 }
 
 
